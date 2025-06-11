2498a2696beeb621fcd84bef3b3ae4ca771decb3
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2025-06-11 22:06:28 +0100

    setup react project

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SearchPage from "./components/SearchPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/search",
+    element: <SearchPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
